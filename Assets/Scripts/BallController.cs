8ad9f743915124f4be64a74caf7a22c3d538910a
Francisco Javier Rubio Jimenez <javirubio_45@hotmail.com>
2022-04-25 11:46:07 -0500

    4.2. Movimiento en el espacio tridimensional

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -18,6 +18,8 @@ public class BallController : MonoBehaviour
             return;
         }
 
+        GameManager.singleton.AddScore(1);
+
         rb.velocity = Vector3.zero;
         rb.AddForce(Vector3.up*impulseForce, ForceMode.Impulse);
 
